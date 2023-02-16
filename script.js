59f9558814a14b9c1acd16b115755df02f768bf0
Spencer Johnson <spencerjcode@gmail.com>
2023-02-16 18:02:29 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function login(element) {
+    if (element.innerText = "Log In") {element.innerText = "Log Out"}
+
+}
+
+function removeOnClick(element) {
+    element.innerText= "Try Again Later :("
+}
